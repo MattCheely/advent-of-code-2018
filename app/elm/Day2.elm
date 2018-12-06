5405ffc1ffa1c55580685d223363cbb489d7ba72
Matthew Cheely <matthew.cheely@genesys.com>
2018-12-06 07:50:52 -0500

    Day 2 - better implementation

--- app/elm/Day2.elm
+++ app/elm/Day2.elm
@@ -91,81 +91,29 @@ partTwo : List String -> String
 partTwo boxIds =
     case boxIds of
         first :: rest ->
-            case searchForMatch first rest of
-                FabricBoxes box1 box2 ->
-                    sharedChars box1 box2
+            case checkBoxAgainstOthers first rest of
+                Just characters ->
+                    characters
 
-                RandoBoxes ->
+                Nothing ->
                     partTwo rest
 
         [] ->
             "Where my boxes at?"
 
 
-type CheckResult
-    = FabricBoxes String String
-    | RandoBoxes
-
-
-searchForMatch : String -> List String -> CheckResult
-searchForMatch box otherBoxes =
-    case otherBoxes of
-        otherBox :: remaining ->
-            case compareBoxes box otherBox of
-                RandoBoxes ->
-                    searchForMatch box remaining
-
-                FabricBoxes box1 box2 ->
-                    FabricBoxes box1 box2
-
-        [] ->
-            RandoBoxes
-
-
-compareBoxes : String -> String -> CheckResult
-compareBoxes box1 box2 =
-    let
-        differences =
-            List.map2 (/=) (String.toList box1) (String.toList box2)
-                |> List.filter identity
-                |> List.length
-    in
-    if differences == 1 then
-        FabricBoxes box1 box2
-
-    else
-        RandoBoxes
-
-
-sharedChars : String -> String -> String
-sharedChars box1 box2 =
-    List.map2
-        (\char1 char2 ->
-            if char1 == char2 then
-                Just char1
-
-            else
-                Nothing
-        )
-        (String.toList box1)
-        (String.toList box2)
-        |> List.filterMap identity
-        |> String.fromList
-
-
-searchForMatchAlt : String -> List String -> Maybe String
-searchForMatchAlt box otherBoxes =
+checkBoxAgainstOthers : String -> List String -> Maybe String
+checkBoxAgainstOthers box otherBoxes =
     case otherBoxes of
         otherBox :: remaining ->
             case
-                doComparison
+                checkBoxes
                     (String.toList box)
                     (String.toList otherBox)
                     []
-                    False
             of
                 Nothing ->
-                    searchForMatchAlt box remaining
+                    checkBoxAgainstOthers box remaining
 
                 Just characters ->
                     Just characters
@@ -174,30 +122,25 @@ searchForMatchAlt box otherBoxes =
             Nothing
 
 
-doComparison : List Char -> List Char -> List Char -> Bool -> Maybe String
-doComparison box1 box2 matches foundDiff =
+checkBoxes : List Char -> List Char -> List Char -> Maybe String
+checkBoxes box1 box2 matches =
     case ( box1, box2 ) of
         ( [], _ ) ->
-            if foundDiff then
-                Just (String.fromList matches)
-
-            else
-                Nothing
+            Nothing
 
         ( _, [] ) ->
-            if foundDiff then
-                Just (String.fromList matches)
-
-            else
-                Nothing
+            Nothing
 
         ( char1 :: rest1, char2 :: rest2 ) ->
-            if char1 == char2 then
-                if not foundDiff then
-                    doComparison rest1 rest1 matches True
+            if char1 /= char2 then
+                if rest1 == rest2 then
+                    List.reverse matches
+                        ++ rest1
+                        |> String.fromList
+                        |> Just
 
                 else
                     Nothing
 
             else
-                doComparison rest1 rest2 (char1 :: matches) foundDiff
+                checkBoxes rest1 rest2 (char1 :: matches)
--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -5,23 +5,27 @@ import Data.Day1 as Day1
 import Data.Day2 as Day2
 import Day1
 import Day2
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, h1, h2, text)
 import Html.Attributes exposing (id)
 
 
 main : Html msg
 main =
-    dayView day2
+    div []
+        [ h1 [] [ text "Advent of code 2018" ]
+        , dayView day2
+        ]
 
 
 dayView day =
     div []
         [ div [ id "part-one" ]
-            [ text ("Part One: " ++ day.partOne)
+            [ h2 [] [ text "Part One" ]
+            , text day.partOne
             ]
-        , div
-            [ id "part-two" ]
-            [ text ("Part Two: " ++ day.partTwo)
+        , div [ id "part-two" ]
+            [ h2 [] [ text "Part Two" ]
+            , text day.partTwo
             ]
         ]
 
